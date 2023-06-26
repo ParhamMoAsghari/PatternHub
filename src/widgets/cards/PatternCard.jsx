c04785c36f2c82a9bc46368248746a6e3df8c7e8
ParhamMoAsghari <parhammohammadasghari@gmail.com>
2023-06-26 20:13:28 +0330

    Added feature to show Base64 images

--- src/widgets/cards/PatternCard.jsx
+++ src/widgets/cards/PatternCard.jsx
@@ -2,12 +2,12 @@ import "./card.css";
 import {Link} from "react-router-dom";
 import ImagePlaceHolder from "@/Tools/ImagePlaceHolder.jsx";
 
-function PatternCard({title, description, imageSRC, link, placeholderSrc, className}) {
+function PatternCard({title, description, imageSRC, imageBase64 = null, link, placeholderSrc, className}) {
     return (
         <Link to={link} key={title}
               className={`card bg-white rounded-lg shadow-lg px-4 transform hover:scale-105 transition-transform duration-300 ${className}`}>
             <ImagePlaceHolder placeholderSrc={placeholderSrc}>
-                <img src={imageSRC} className="rounded pb-2" style={{width: 280 + 'px', height: 200 + 'px',}}
+                <img src={imageBase64 !== null ?`data:image/jpeg;base64,${imageBase64}`: imageSRC} className="rounded pb-2" style={{width: 280 + 'px', height: 200 + 'px',}}
                      alt={title}/>
             </ImagePlaceHolder>
             <h2 className="text-xl font-semibold mb-2">{title}</h2>
