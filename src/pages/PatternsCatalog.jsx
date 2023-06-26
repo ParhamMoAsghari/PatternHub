29847783b6858b9d2fe526a3d2055aba02f45133
ParhamMoAsghari <parhammohammadasghari@gmail.com>
2023-06-26 20:14:10 +0330

    Gets patterns list from server

--- src/pages/PatternsCatalog.jsx
+++ src/pages/PatternsCatalog.jsx
@@ -1,7 +1,8 @@
-import "./CardTransition.css"
 import PatternCard from "@/widgets/cards/PatternCard.jsx";
-import {useEffect, useRef} from "react";
 import {useParams} from "react-router-dom";
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
+import serverUrl from "@/config.js";
 
 const types = (
     <>
@@ -23,96 +24,66 @@ const types = (
     </>
 )
 
-const structural = (
-    <>
-        <PatternCard
-            title="Adapter"
-            description="الگوی آداپتور به شما امکان می‌دهد رابطه‌ی ساختاری بین دو کلاس مختلف را تطبیق دهید."
-            imageSRC="/img/Adapter.jpg"
-            link="/pattern/structural/adapter"/>
-        <PatternCard
-            title="Bridge"
-            description="الگوی پل به شما امکان می‌دهد ارتباط بین اجزاء مختلف سیستم را جدا کرده و قابلیت تغییرات و انعطاف‌پذیری را فراهم کنید."
-            imageSRC="/img/Bridge.jpg"
-            link="/pattern/structural/bridge"/>
-        <PatternCard
-            title="Composite"
-            description="الگوی کامپوزیت به شما اجازه می‌دهد اشیاء را در ساختارهای سلسله‌مراتبی یکپارچه کنید و به آن‌ها به عنوان یک واحد عمل کنید."
-            imageSRC="/img/Composite.jpg"
-            link="/pattern/structural/composite"/>
-    </>
-)
-
-const behavioral = (
-    <>
-        <PatternCard
-            title="Chain of Responsibility"
-            description="الگوهایی که به شما کمک می‌کنند نحوه ایجاد شیء را بهینه‌تر مدیریت کنید"
-            imageSRC="/img/ChainOfResponsibility.jpg"
-            placeholderSrc={"/img/small/ChainOfResponsibility.png"}
-            link="/pattern/behavioral/chainOfResponsibility"/>
-        <PatternCard
-            title="Command"
-            description="الگوهایی که به شما کمک می‌کنند ساختارهای مختلف برنامه را بهبود بخشید"
-            imageSRC="/img/Command.jpg"
-            link="/pattern/behavioral/command"/>
-        <PatternCard
-            title="Interpreter"
-            description="الگوهایی که بر رفتار و ارتباط بین اشیاء متمرکز می‌شوند"
-            imageSRC="/img/Interpreter.jpg"
-            placeholderSrc={"/img/small/Interpreter.jpg"}
-            link="/pattern/behavioral/interpreter"/>
-    </>
-)
-const creational = (
-    <>
-        <PatternCard
-            title="Abstract Factory"
-            description="الگوهایی که به شما کمک می‌کنند نحوه ایجاد شیء را بهینه‌تر مدیریت کنید"
-            imageSRC="/img/AbstractFactory.jpg"
-            link="/pattern/creational/abstractFactory"/>
-        <PatternCard
-            title="Builder"
-            description="الگوهایی که به شما کمک می‌کنند ساختارهای مختلف برنامه را بهبود بخشید"
-            imageSRC="/img/Builder.avif"
-            link="/pattern/creational/builder"/>
-        <PatternCard
-            title="Factory Method"
-            description="الگوهایی که بر رفتار و ارتباط بین اشیاء متمرکز می‌شوند"
-            imageSRC="/img/FactoryMethod.jpg"
-            link="/pattern/creational/factoryMethod"/>
-    </>
-)
-
 export function PatternsCatalog() {
-    const {patternType} = useParams();
+    const { patternType } = useParams();
     const cardContainerRef = useRef(null);
+    const [patterns, setPatterns] = useState([]);
+
     useEffect(() => {
         const cardContainer = cardContainerRef.current;
         cardContainer.classList.add("card-enter");
+
+        // Fetch pattern data from the server
+        const fetchPatterns = async () => {
+            try {
+                if (patternType !== undefined) {
+                    const response = await axios.get(
+                        serverUrl + `/patterns/list?type=${patternType}`
+                    );
+                    setPatterns(response.data);
+                } else {
+                    setPatterns(types);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchPatterns();
     }, [patternType]);
+
     return (
         <>
             <section className="relative block h-[50vh]">
                 <div
-                    className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/DesignPatterns.jpg')] bg-cover bg-center"/>
-                <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center"/>
+                    className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/DesignPatterns.jpg')] bg-cover bg-center"
+                />
+                <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
             </section>
             <section className="relative bg-blue-gray-50/50 py-16 px-4 content-center">
                 <div className="container mx-auto">
-                        <div className="flex items-center justify-center bg-gray-200 -mt-64 rounded-3xl">
-                            <div ref={cardContainerRef}
-                                 className="grid sm:grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 my-12 card-container justify-center align-middle">
-                                {patternType === undefined && types}
-                                {patternType === "structural" && structural}
-                                {patternType === "behavioral" && behavioral}
-                                {patternType === "creational" && creational}
-                            </div>
+                    <div className="flex items-center justify-center bg-gray-200 -mt-64 rounded-3xl">
+                        <div
+                            ref={cardContainerRef}
+                            className="grid sm:grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 my-12 card-container justify-center align-middle"
+                        >
+                            {patternType && patterns.length > 0 ? (
+                                patterns.map((pattern) => (
+                                    <PatternCard
+                                        key={pattern.name}
+                                        title={pattern.name}
+                                        description={pattern.description}
+                                        imageBase64={pattern.image}
+                                        link={`/pattern/${patternType}/${pattern.name}`}
+                                    />
+                                ))
+                            ) : (
+                                patternType === undefined && types
+                            )}
                         </div>
                     </div>
+                </div>
             </section>
         </>
     );
 }
-
-export default PatternsCatalog;
