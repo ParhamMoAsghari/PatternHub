2138596b119f8bed85cb37ff4a8ebfdb5c95305d
ParhamMoAsghari <parhammohammadasghari@gmail.com>
2023-06-27 10:20:47 +0330

    Added image placeholder

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+//const serverUrl = 'https://patternhubback.iran.liara.run';
 const serverUrl = 'http://localhost:3000';
+
 
 export default serverUrl;
--- src/pages/PatternsCatalog.jsx
+++ src/pages/PatternsCatalog.jsx
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import serverUrl from "@/config.js";
+import ImagePlaceHolder from "@/Tools/ImagePlaceHolder.jsx";
 
 const types = (
     <>
@@ -25,9 +26,11 @@ const types = (
 )
 
 export function PatternsCatalog() {
-    const { patternType } = useParams();
+    const {patternType} = useParams();
     const cardContainerRef = useRef(null);
     const [patterns, setPatterns] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         const cardContainer = cardContainerRef.current;
@@ -41,7 +44,9 @@ export function PatternsCatalog() {
                         serverUrl + `/patterns/list?type=${patternType}`
                     );
                     setPatterns(response.data);
+                    setIsLoading(false);
                 } else {
+                    setIsLoading(false);
                     setPatterns(types);
                 }
             } catch (error) {
@@ -58,7 +63,7 @@ export function PatternsCatalog() {
                 <div
                     className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/DesignPatterns.jpg')] bg-cover bg-center"
                 />
-                <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
+                <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center"/>
             </section>
             <section className="relative bg-blue-gray-50/50 py-16 px-4 content-center">
                 <div className="container mx-auto">
@@ -67,19 +72,28 @@ export function PatternsCatalog() {
                             ref={cardContainerRef}
                             className="grid sm:grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 my-12 card-container justify-center align-middle"
                         >
-                            {patternType && patterns.length > 0 ? (
-                                patterns.map((pattern) => (
-                                    <PatternCard
-                                        key={pattern.name}
-                                        title={pattern.name}
-                                        description={pattern.description}
-                                        imageBase64={pattern.image}
-                                        link={`/pattern/${patternType}/${pattern.name}`}
-                                    />
-                                ))
+                            {isLoading ? (
+                                <div className="flex justify-center items-center h-full">
+                                    <p>Loading...</p>
+                                </div>
                             ) : (
-                                patternType === undefined && types
+                                patternType && patterns.length > 0 ? (
+                                    patterns.map((pattern) => (
+                                        <ImagePlaceHolder placeholderSrc={""}>
+                                            <PatternCard
+                                                key={pattern.name}
+                                                title={pattern.name}
+                                                description={pattern.description}
+                                                imageBase64={pattern.image}
+                                                link={`/pattern/${patternType}/${pattern.name}`}
+                                            />
+                                        </ImagePlaceHolder>
+                                    ))
+                                ) : (
+                                    patternType === undefined && types
+                                )
                             )}
+
                         </div>
                     </div>
                 </div>
